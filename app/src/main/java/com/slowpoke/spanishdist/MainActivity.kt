09cfb5b3d3dc44fc0ced8abbad49ed59485948cd
Amina <amina.muradalieva91@gmail.com>
2023-10-25 16:28:02 +0600

    Added check for 'word not found'. Added progressBar. Still no list though

--- app/src/main/java/com/slowpoke/spanishdist/MainActivity.kt
+++ app/src/main/java/com/slowpoke/spanishdist/MainActivity.kt
@@ -6,9 +6,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.annotation.RequiresExtension
+import androidx.core.view.isVisible
 import androidx.lifecycle.lifecycleScope
 import com.slowpoke.spanishdist.databinding.ActivityMainBinding
+import com.slowpoke.spanishdist.dictWordEntry.WordEntry
 import kotlinx.coroutines.launch
+import retrofit2.Response
 import java.io.IOException
 
 const val TAG = "RetrofitActivity"
@@ -20,27 +23,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.progressBar.isVisible = false;
 
         binding.btnSearch.setOnClickListener {
             val searchWord = binding.etSearchWord.text.toString()
 
             lifecycleScope.launch {
-
+                binding.progressBar.isVisible = true;
                 val response = try {
                     RetrofitInstance.api.getDictEntry(searchWord, RetrofitInstance.API_KEY)
                 } catch (e: IOException) {
                     Log.d(TAG, "Check your internet connection")
+                    binding.progressBar.isVisible = false;
                     return@launch
                 } catch (e: HttpException) {
                     Log.d(TAG, "Server response unsuccessful")
+                    binding.progressBar.isVisible = false;
                     return@launch
                 }
-
-                if (response.isSuccessful && response.body() != null) {
+                binding.progressBar.isVisible = false;
+                if (response.isSuccessful && response.body() != null && response.body()!!.isNotEmpty()
+                ) {
                     val firstWordEntry = response.body()!![0]
                     val translation = firstWordEntry.shortdef[0]
                     binding.tvTranslation.text = translation
-                }
+                } else
+                    binding.tvTranslation.text = getString(R.string.entry_not_found)
 
             }
         }
